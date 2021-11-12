90f55d9d4135aeb40c8906a31b5dc270f464097e
Muhammad Musaab Ul Haq <71430990+MuhammadMusaabUlHaq@users.noreply.github.com>
2021-11-12 15:23:06 +0500

    Basic logic changes

--- scripts/story-gen.js
+++ scripts/story-gen.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = 'Life was so harsh. Aline never knew about it. It was a monday, when annoyed by a tantrum. She left the house. What she did not understood back then, :insertx: ';
+let insertX = ['that life is a never ending cycle of obtaining knowledge.','that we only value things once they are lost.','that things with value are always not cared for until they are lost.'];
+let insertY = ['She wanted to ask, why? Why must our heads be empty when we are borned?','She wanted to curse the gods, why are they born with such low intellect.','She was annoyed that she did not know about it.'];
+let insertZ = ['Nights went by as she ate what she packed and went around, thinking for an answer. With no results she gave up and left for a journey...','After days of thinking while being stranded, she gave up and decided that its best to ask those who have lived longer than me. Thus her journey started, with one that would leave many dumbfounded... ','In the end, there is no answer. Thats why the humans have intellect. We must find it ourselves. And so she embarked on a journey...'];
 
 randomize.addEventListener('click', result);
 
@@ -28,14 +28,7 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob',name);
-  }
-
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300*0.0714286) + ' stone';
-    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-    newStory = newStory.replace('94 fahrenheit',temperature);
-    newStory = newStory.replace('300 pounds',weight);
+    newStory = newStory.replace('Aline',name);
   }
 
   story.textContent = newStory;
